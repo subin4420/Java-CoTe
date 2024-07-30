388451fb53f4cd73c7fa535d64ad678c48f77bf8
dh1010a <dh1010a@naver.com>
2024-07-30 14:29:28 +0900

    add: 백도현/week9/pgs1844, 12952, 43162, 43163, 43165, 81302, 159993

--- src/백도현/week9/9-1/pgs43163_dh.java
+++ src/백도현/week9/9-1/pgs43163_dh.java
@@ -41,3 +41,56 @@ class Solution {
         return origin.length() -1 == tmp ? true : false;
     }
 }
+
+import java.util.*;
+class Solution {
+    
+    public int solution(String begin, String target, String[] words) {
+        boolean check = false;
+        for(String x: words) {
+            if (x.equals(target)) check = true;
+        }
+        if (!check) return 0;
+        
+        Deque<Node> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[words.length];
+        
+        q.addFirst(new Node(begin, 0));
+        while(!q.isEmpty()) {
+            Node now = q.pollLast();
+            if(now.str.equals(target)) {
+                return now.cnt;
+            }
+            for(int i = 0; i < words.length; i++) {
+                if(!visited[i] && isChangeable(words[i], now.str)) {
+                    visited[i] = true;
+                    q.addFirst(new Node(words[i], now.cnt + 1));
+                }
+            }
+        }
+        
+        
+        return 0;
+    }
+    
+    
+    public boolean isChangeable(String origin, String target) {
+        int tmp = 0;
+        for (int i = 0; i < origin.length(); i++) {
+            if (origin.charAt(i) == target.charAt(i)) {
+                tmp++;
+            }
+        }
+        return origin.length() -1 == tmp ? true : false;
+    }
+    
+    class Node{
+        String str;
+        int cnt;
+        
+        public Node(String str, int cnt) {
+            this.str=str;
+            this.cnt=cnt;
+        }
+    }
+}
